a645afb4e3b41db5a7aeb278f1e0639ef98ddfca
Sapan Bhuta <sapanbhuta@gmail.com>
2014-12-28 22:38:52 -0500

    init

---
+++ Mail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mail
+//
+//  Created by Sapan Bhuta on 12/28/14.
+//  Copyright (c) 2014 SapanBhuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
